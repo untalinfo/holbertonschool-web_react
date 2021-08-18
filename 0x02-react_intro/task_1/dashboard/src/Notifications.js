846e2a985e76ed6c9496f410aeaa38c545598bab
untalinfo <racso1607@gmail.com>
2021-08-18 00:06:55 -0500

    resolve problem

---
+++ 0x02-react_intro/task_1/dashboard/src/Notifications.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import './Notifications.css'
+
+export default class Notifications extends Component {
+  render() {
+    return <div className="Notifications">
+      <p>Here is the list of notifications</p>
+    </div>
+  }
+}
